881bcdee8303727c9c2577e20e4d15a72450e553
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2024-02-21 18:40:16 +0530

    day 03 defult props , new comp : TodoItem & Todos added .
    1. default props in header comp .
    2. created Todos object in App.js .
    3. created Todos & TodoItem comp .
    4. applied map method in Todos comp .
    5. create delete method in app.js
    6. applied delete method in TodoItem comp

--- React.js/my-react-app/src/App.js
+++ React.js/my-react-app/src/App.js
@@ -1,10 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './MyComponent/Header';
+import Todos from  './MyComponent/Todos';
 
 function App() {
+
+  const onDelete = (todo) =>{
+    console.log("I am ondelete of todo")
+  }
+
+  let todos = [
+    {
+      sno : 1 , 
+      title : "Go to the gym",
+      desc : "Yon need to go to the gym for exercise "
+    },
+    {
+      sno : 2 , 
+      title : "Go to the Market",
+      desc : "Yon need to go to the Market for shopping "
+    },
+    {
+      sno : 3 , 
+      title : "Go to the college",
+      desc : "Yon need to go to the gym for studies "
+    }
+  ]
+
   return (
-    <Header title = {67} searchBar={false}/>
+    <>
+    <Header title = {'Title'} searchBar={false}/>
+    <Todos todos = {todos} onDelete={onDelete}/>
+    </>
   );
 }
 
