abfbe602fb979a6043114e1f7f439fed99646ce7
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2024-02-23 21:58:57 +0530

    Add module import and function call
    1. Imported sayHello function and greeting variable from module1.js into module2.js.
    
    2. Utilized sayHello function and logged greeting variable to the console for display.

---
+++ Javascript/07_/module1.js
@@ -0,0 +1,15 @@
+//Function
+
+function sayHello(){
+    console.log("Hello from module1")
+}
+
+// variable
+
+const greeting = "Welcome";
+
+
+module.exports = {
+    sayHello,
+    greeting
+}
---
+++ Javascript/07_/module2.js
@@ -0,0 +1,4 @@
+const {sayHello, greeting}  = require('./module1.js')
+
+sayHello();
+console.log(greeting)
