024a6b7ea48cc663f59a71c5b81c15a71ab3f8ac
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2024-02-26 21:34:29 +0530

    2nd folder merged

---
+++ React.js/02 Todos CRUD App - React/my_todos_app/src/myComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      Hello from Todo component
+    </div>
+  )
+}
+
+export default Todo
---
+++ React.js/02 Todos CRUD App - React/my_todos_app/src/myComponents/TodoListView.js
@@ -0,0 +1,13 @@
+import Todo from "./Todo";
+
+import React from 'react'
+
+const TodoListView = () => {
+  return (
+    <div>
+      Hello from TodoListView component
+    </div>
+  )
+}
+
+export default TodoListView
