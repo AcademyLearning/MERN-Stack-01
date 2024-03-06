148f71758a9c119c4e696bd66dd467e363661987
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2024-03-06 17:45:23 +0530

    Todo update functionality added .
    Note :  The PUT request is pending; it still needs to be fulfilled.

--- React.js/02 Todos CRUD App - React/my_todos_app/src/myComponents/Todo.js
+++ React.js/02 Todos CRUD App - React/my_todos_app/src/myComponents/Todo.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Todo = (props) => {
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState(props.todo.title);
+  const [updatedDescription, setUpdatedDescription] = useState(props.todo.description);
 
+  
   return (
     <div>
-     <p>
-     <span style={{ fontWeight:'bold' }} > {props.todo.title} </span> : {props.todo.description}
-     <button onClick={() => props.deleteTodoHandler(props.todo.title)} className='btn btn-outline-danger my-2 mx-2'  >delete</button>
-    <hr />
-     </p>
+      {isEditing ? (
+          
+     <>
+     <input onChange={(e) => setUpdatedTitle(e.target.value)} value={updatedTitle} type="text" />
+     <input onChange={(e) => setUpdatedDescription(e.target.value)} value={updatedDescription} type="text" />
+     <button onClick={updateTodoHandler} className='btn btn-outline-success my-2 mx-2'>Update</button>
+      </>
+      ) : (
+        <p>
+          <span style={{ fontWeight: 'bold' }} > {props.todo.title} </span> : {props.todo.description}
+          <button onClick={() => props.deleteTodoHandler(props.todo.title)} className='btn btn-outline-danger my-2 mx-2'  >delete</button>
+          <button onClick={() => setIsEditing(true)} className='btn btn-outline-primary my-2 mx-2'  >Edit</button>
+          <hr />
+        </p>
+      )}
+
+
+
     </div>
   )
 }
